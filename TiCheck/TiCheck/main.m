c71b1751c63c414867a234f8ff6cd12648461c9c
Boyi Lee <boylee1111@gmail.com>
2014-03-17 20:38:53 +0800

    new TiCheck_Client git repo

---
+++ TiCheck/TiCheck/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TiCheck
+//
+//  Created by Boyi on 3/17/14.
+//  Copyright (c) 2014 tac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
